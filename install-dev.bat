37e077c75763c455782700c7f7c47ff673387859
pozil <philippe.ozil@gmail.com>
2020-01-07 14:23:58 +0100

    Added Windows install script

---
+++ install-dev.bat
@@ -0,0 +1,40 @@
+@echo OFF
+
+set ORG_ALIAS=ui-api
+
+@echo:
+echo Installing UI API Playground org (%ORG_ALIAS%)
+@echo:
+
+rem Install script
+echo Cleaning previous scratch org...
+cmd.exe /c sfdx force:org:delete -p -u %ORG_ALIAS% 2>NUL
+@echo:
+
+echo Creating scratch org...
+cmd.exe /c sfdx force:org:create -s -f config/project-scratch-def.json -a %ORG_ALIAS% -d 30
+call :checkForError
+@echo:
+
+echo Pushing source...
+cmd.exe /c sfdx force:source:push -f -u %ORG_ALIAS%
+call :checkForError
+@echo:
+
+rem Check exit code
+@echo:
+if ["%errorlevel%"]==["0"] (
+  echo Installation completed.
+  @echo:
+  cmd.exe /c sfdx force:org:open -u %ORG_ALIAS%
+)
+
+:: ======== FN ======
+GOTO :EOF
+
+rem if the app has failed
+:checkForError
+if NOT ["%errorlevel%"]==["0"] (
+    echo Installation failed.
+    exit /b %errorlevel%
+)
