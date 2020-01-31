18a8467e715fc90a43775e337527c1450f86d99f
pozil <philippe.ozil@gmail.com>
2020-01-31 11:48:19 +0100

    Refactored UI: switched from tabs to menus

---
+++ src/main/default/lwc/uiApiPlayground/api-definitions.js
@@ -0,0 +1,58 @@
+const API_DEFINITIONS = [
+    {
+        name: 'uiListApi',
+        endpoints: [
+            {
+                name: 'getListUi',
+                description:
+                    "Use this wire adapter to get the records and metadata for a list view. Leave the 'List View API Name' field empty to retrieve all list views.",
+                docUrlKey: 'lwc.reference_get_list_ui'
+            }
+        ]
+    },
+    {
+        name: 'uiObjectInfoApi',
+        endpoints: [
+            {
+                name: 'getObjectInfo',
+                description:
+                    'Use this wire adapter to get metadata about a specific object. The response includes metadata describing fields, child relationships, record type, and theme.',
+                docUrlKey: 'lwc.reference_wire_adapters_object_info'
+            },
+            {
+                name: 'getPicklistValues',
+                description:
+                    'Use this wire adapter to get the picklist values for a specified field.',
+                docUrlKey: 'lwc.reference_wire_adapters_picklist_values'
+            },
+            {
+                name: 'getPicklistValuesByRecordType',
+                description:
+                    'Use this wire adapter to get the values for every picklist of a specified record type.',
+                docUrlKey: 'lwc.reference_wire_adapters_picklist_values_record'
+            }
+        ]
+    },
+    {
+        name: 'uiRecordApi',
+        endpoints: [
+            {
+                name: 'getRecordCreateDefaults',
+                description:
+                    'Use this wire adapter to get default information and data needed to create a record.',
+                docUrlKey: 'lwc.reference_wire_adapters_create_record_values'
+            }
+        ]
+    }
+];
+
+// Add fullname to endpoints
+const FULL_API_DEFINITIONS = API_DEFINITIONS.map(category => {
+    category.endpoints = category.endpoints.map(endpoint => {
+        endpoint.fullname = `${category.name}.${endpoint.name}`;
+        return endpoint;
+    });
+    return category;
+});
+
+export default FULL_API_DEFINITIONS;
--- src/main/default/lwc/uiApiPlayground/uiApiPlayground.js
+++ src/main/default/lwc/uiApiPlayground/uiApiPlayground.js
@@ -1,3 +1,34 @@
 import { LightningElement } from 'lwc';
+import API_DEFINITIONS from './api-definitions.js';
 
-export default class UiApiPlayground extends LightningElement {}
+export default class UiApiPlayground extends LightningElement {
+    apiDefinitions = API_DEFINITIONS;
+    selectedEndpoint;
+
+    handleMenuSelect(event) {
+        const { value } = event.detail;
+        this.selectedEndpoint = this.apiDefinitions
+            .reduce(
+                (accumulator, category) =>
+                    accumulator.concat(category.endpoints),
+                []
+            )
+            .find(item => item.name === value);
+    }
+
+    get isGetListUi() {
+        return this.selectedEndpoint.name === 'getListUi';
+    }
+    get isGetObjectInfo() {
+        return this.selectedEndpoint.name === 'getObjectInfo';
+    }
+    get isGetPicklistValues() {
+        return this.selectedEndpoint.name === 'getPicklistValues';
+    }
+    get isGetPicklistValuesByRecordType() {
+        return this.selectedEndpoint.name === 'getPicklistValuesByRecordType';
+    }
+    get isGetRecordCreateDefaults() {
+        return this.selectedEndpoint.name === 'getRecordCreateDefaults';
+    }
+}
