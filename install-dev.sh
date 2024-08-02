70601be192fe7b4c1159726f7ab562207693b031
Philippe Ozil <pozil@salesforce.com>
2024-08-02 10:21:03 +0200

    build: upgrade to sf CLI

--- install-dev.sh
+++ install-dev.sh
@@ -10,19 +10,19 @@ echo ""
 
 # Install script
 echo "Cleaning previous scratch org..."
-sfdx force:org:delete -p -u $ORG_ALIAS &> /dev/null
+sf org delete scratch -p -o $ORG_ALIAS &> /dev/null
 echo ""
 
 echo "Creating scratch org..." && \
-sfdx force:org:create -s -f config/project-scratch-def.json -a $ORG_ALIAS -d 30 && \
+sf org create scratch -f config/project-scratch-def.json -a $ORG_ALIAS -d -y 30 && \
 echo "" && \
 
 echo "Pushing source..." && \
-sfdx force:source:push -f -u $ORG_ALIAS && \
+sf project deploy start && \
 echo "" && \
 
 echo "Assigning permissions..." && \
-sfdx force:user:permset:assign -n UI_API_Playground -u $ORG_ALIAS && \
+sf org assign permset -n UI_API_Playground && \
 echo ""
 
 EXIT_CODE="$?"
@@ -32,7 +32,7 @@ echo ""
 if [ "$EXIT_CODE" -eq 0 ]; then
   echo "Installation completed."
   echo ""
-  sfdx force:org:open -p lightning/n/UI_API_Playground -u $ORG_ALIAS
+  sf org open -p lightning/n/UI_API_Playground
 else
     echo "Installation failed."
 fi
